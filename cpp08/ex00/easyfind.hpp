bfcef4310dff1768a109b063f0d82178058eccb1
jollagit <giuliaviga@gmail.com>
2025-10-14 16:40:28 +0200

    cpp08/ex00 updated

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gvigano <gvigano@student.42.fr>            +#+  +:+       +#+        */
+/*   By: giuliaviga <giuliaviga@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 14:41:33 by gvigano           #+#    #+#             */
-/*   Updated: 2025/10/09 14:41:34 by gvigano          ###   ########.fr       */
+/*   Updated: 2025/10/14 16:37:33 by giuliaviga       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,23 @@ public:
 };
 
 template<typename T>
-typename T::iterator easyfind(T& container, int value) {
-	for (typename T::iterator i = container.begin();
-		i != container.end(); ++i) {
-			if (*i == value )
-				return i;
-		}
-	throw NotInThereException();
+typename T::iterator	easyfind(T& container, int value) {
+	typename T::iterator it = find(container.begin(), container.end(), value);
+	if (it != container.end())
+		return it;
+	else
+		throw NotInThereException();
 }
 
+// template<typename T>
+// typename T::iterator easyfind(T& container, int value) {
+// 	for (typename T::iterator i = container.begin();
+// 		i != container.end(); ++i) {
+// 			if (*i == value )
+// 				return i;
+// 		}
+// 	throw NotInThereException();
+// }
 
 
 #endif
